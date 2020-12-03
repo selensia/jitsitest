3052d3788801cb356d3724c7e43afee3e45096f7
Eduardo Martins Lima <doneduardold@gmail.com>
2020-12-03 15:21:43 -0300

    Add files via upload

---
+++ clinica/main/php/entidade/Medico.php
@@ -0,0 +1,7 @@
+<?php
+
+class Medico
+{
+    public $id;
+    public $nomeMedico;
+}
---
+++ clinica/main/php/entidade/Sala.php
@@ -0,0 +1,11 @@
+<?php
+
+class Sala
+{
+    public $id;
+    public $medico;
+    public $nomeSala;
+    public $senha;
+    public $nomePaciente;
+    public $token;
+}
